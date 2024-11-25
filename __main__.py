75bc6065532ece2c1051d9e23b892c4727d68485
Konrad Grochowski <Konrad.Grochowski@pw.edu.pl>
2024-11-25 17:48:40 +0100

    basic validator added

--- __main__.py
+++ __main__.py
@@ -2,15 +2,19 @@ import logging
 import sys
 import time
 
+from .coapp import Validator
 from .net import Loop
 
 
 def main():
     logging.basicConfig(level=logging.DEBUG)
 
-    with Loop(None) as loop:  # TODO
-        loop.send(("127.0.0.1", 2000), b"aaaaa")
-        time.sleep(10)
+    addr = ("127.0.0.1", 2000)  # TODO
+    validator = Validator(addr)
+
+    with Loop(validator) as loop:  # TODO
+        loop.send(addr, b"aaaaa")
+        validator.wait_for_validation()
     return 0
 
 
