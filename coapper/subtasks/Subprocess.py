51d6a66a35411bbf0f28e1174d46517753dd0b31
Konrad Grochowski <Konrad.Grochowski@pw.edu.pl>
2025-02-10 18:58:10 +0100

    "subtask" submodule added, not connected yet

---
+++ coapper/subtasks/Subprocess.py
@@ -0,0 +1,57 @@
+import logging
+import subprocess
+from typing import Self
+
+from ..Config import Config
+from . import SubTask
+
+logger = logging.getLogger(__name__)
+
+
+class Subprocess(SubTask):
+    def __init__(
+        self,
+        args: list[str],
+        timeout: float,
+        shell: bool = False,
+        name: str = "subprocess",
+    ):
+        super().__init__(name)
+
+        self.args = args
+        self.timeout = timeout
+        self.shell = shell
+
+    def run(self) -> SubTask.Result:
+        logger.info(f"<{self.name()}>: Calling `{' '.join(self.args)}`")
+        try:
+            result = subprocess.run(
+                self.args,
+                timeout=self.timeout,
+                shell=self.shell,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.STDOUT,
+            )
+        except subprocess.TimeoutExpired:
+            logger.warn(f"<{self.name()}: Operation timedout")
+            return self.Result.TIMEOUT
+        except Exception as ex:
+            logger.error(f"<{self.name()}>: Operation error", ex)
+            return self.Result.ERROR
+
+        if result.returncode != 0:
+            logger.warn(f"<{self.name()}>: Operation returned {result.returncode}")
+            logger.warn(result.stdout)
+            return self.Result.FAILURE
+
+        logger.info(f"<{self.name()}>: Operation finished")
+        return self.Result.SUCCESS
+
+    @classmethod
+    def from_config(cls, name: str, config: Config) -> Self:
+        return cls(
+            name=name,
+            args=config.get_str_list("cmd"),
+            timeout=config.get_float("timeout"),
+            shell=config.get_bool("shell"),
+        )
---
+++ coapper/subtasks/__init__.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+from enum import StrEnum, auto
+
+from ..Config import Config
+
+
+class SubTask(ABC):
+
+    class Result(StrEnum):
+        SUCCESS = auto()
+        FAILURE = auto()
+        TIMEOUT = auto()
+        ERROR = auto()
+
+    def __init__(self, name: str):
+        self._name = name
+
+    def name(self) -> str:
+        return self._name
+
+    @abstractmethod
+    def run(self) -> Result: ...
+
+
+def from_config(config: Config) -> SubTask:
+    type = config.get_str("type")
+    name = config.get_str("name")
+    args = config.section("args")
+    match type:
+        case "subprocess":
+            from .Subprocess import Subprocess
+
+            return Subprocess.from_config(name, args)
+        case _:
+            raise ValueError(f"Unknown sub-task type '{type}'")
