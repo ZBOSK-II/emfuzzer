73c26ed2aaffafb22278c57f199fa9d8364acd98
Konrad Grochowski <Konrad.Grochowski@pw.edu.pl>
2025-03-17 15:16:51 +0100

    coapp.Validator refactored to remove race
    now every sensitive operation is performed inside "with condition var"

--- coapper/coapp/Validator.py
+++ coapper/coapp/Validator.py
@@ -2,8 +2,7 @@ import logging
 import threading
 from enum import StrEnum, auto
 
-from ..net import Address
-from ..net import Consumer
+from ..net import Address, Consumer
 from .code import code_reports_success, code_to_string, decode_code
 
 logger = logging.getLogger(__name__)
@@ -23,21 +22,20 @@ class Validator(Consumer):
         self.expected_ip = expected_ip
         self.timeout = timeout
 
-        self.expecting_event = threading.Event()
-        self.validation_complete_cv = threading.Condition()
-
-        self.last_result: Validator.Result = self.Result.UNKNOWN
+        self.cond = threading.Condition()
+        self.expecting = False
+        self.result: Validator.Result = self.Result.UNKNOWN
 
         self.unexpected_messages = 0
 
     def on_received(self, addr: Address, data: bytes) -> None:
-        if not self.expecting_event.is_set():
-            self.__unexpected_message()
-            return
-        self.expecting_event.clear()
-        self.last_result = self.check_message(addr, data)
-        with self.validation_complete_cv:
-            self.validation_complete_cv.notify()
+        with self.cond:
+            if not self.expecting:
+                self.__unexpected_message()
+                return
+            self.expecting = False
+            self.result = self.check_message(addr, data)
+            self.cond.notify()
 
     def check_message(self, addr: Address, data: bytes) -> Result:
         if addr != self.expected_ip:
@@ -61,21 +59,21 @@ class Validator(Consumer):
         return self.Result.SUCCESS
 
     def on_sent(self, addr: Address, data: bytes) -> None:
-        self.last_result = self.Result.UNKNOWN
-        self.expecting_event.set()
+        with self.cond:
+            self.expecting = True
+            self.result = self.Result.UNKNOWN
 
     def wait_for_result(self) -> Result:
-        with self.validation_complete_cv:
-            if not self.validation_complete_cv.wait_for(
-                lambda: self.last_result != self.Result.UNKNOWN, timeout=self.timeout
+        with self.cond:
+            if not self.cond.wait_for(
+                lambda: self.result != self.Result.UNKNOWN, timeout=self.timeout
             ):
+                self.expecting = False
                 logger.warn("Operation timed out")
-                self.expecting_event.clear()
                 return self.Result.TIMEDOUT
-
-        result = self.last_result
-        self.last_result = self.Result.UNKNOWN
-        return result
+            result = self.result
+            self.result = self.Result.UNKNOWN
+            return result
 
     def extra_stats(self) -> dict[str, int]:
         return {"unexpected_messages": self.unexpected_messages}
